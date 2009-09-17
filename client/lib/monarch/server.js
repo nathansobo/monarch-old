47e497bdde6d0216f7b9c8c575371465bda94ad2
Nathan Sobo <nathansobo+monarch@gmail.com>
2009-09-17 15:49:09 -0700

    Extracted from ce3 branch at rev 32275fd5ce7bda1f192603f0e17de1cc194fac54

---
+++ client/lib/monarch/server.js
@@ -0,0 +1,54 @@
+constructor("Server", {
+  fetch: function(url, relations) {
+    var fetch_future = new Model.FetchFuture();
+
+    Origin.get(url, {
+      relations: Util.map(relations, function(relation) {
+        return relation.wire_representation();
+      })
+    })
+      .on_success(function(data) {
+        Repository.pause_delta_events();
+        Repository.update(data);
+        fetch_future.trigger_before_delta_events();
+        Repository.resume_delta_events();
+        fetch_future.trigger_after_delta_events();
+      });
+
+    return fetch_future;
+  },
+
+  post: function(url, data) {
+    return this.request('POST', url, data);
+  },
+
+  get: function(url, data) {
+    return this.request('GET', url, data);
+  },
+
+  put: function(url, data) {
+    return this.request('PUT', url, data);
+  },
+
+  request: function(type, url, data) {
+    var future = new AjaxFuture();
+    jQuery.ajax({
+      url: url,
+      type: type,
+      dataType: 'json',
+      data: this.stringify_json_data(data),
+      success: function(response) {
+        future.handle_response(response);
+      }
+    });
+    return future;
+  },
+
+  stringify_json_data: function(data) {
+    var stringified_data = {};
+    Util.each(data, function(key, value) {
+      stringified_data[key] = JSON.stringify(value);
+    });
+    return stringified_data;
+  }
+});
