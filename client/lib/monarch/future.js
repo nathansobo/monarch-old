e2a54015d8e1a98ce21d778374bc2ce946cd42e9
Nathan Sobo <nathansobo+monarch@gmail.com>
2009-09-30 11:57:38 -0700

    Added Future#propagate_completion

--- client/lib/monarch/future.js
+++ client/lib/monarch/future.js
@@ -11,6 +11,16 @@ constructor("Future", {
     }
   },
 
+  propagate_completion: function(future) {
+    if (this.completed) {
+      future.complete(this.value)
+    } else {
+      return this.on_complete_node.subscribe(function(value) {
+        future.complete(value);
+      });
+    }
+  },
+
   complete: function(value) {
     this.completed = true;
     this.value = value;
