704fbd1e754f21ddefbbb8540f98a4bc24b69682
Nathan Sobo <nathansobo+monarch@gmail.com>
2009-10-20 16:14:15 -0700

    JoinRecord now uses Table#build_record_from_database to build the constituent records, which makes it honor the identity map

--- server/lib/monarch/model/join_record.rb
+++ server/lib/monarch/model/join_record.rb
@@ -14,19 +14,28 @@ module Model
       end
     end
 
+    def [](table_or_record_class)
+      constituent_records_by_table[table_or_record_class.table]
+    end
+
+    def inspect
+      constituent_records_by_table_name = {}
+      constituent_records_by_table.each do |table, record|
+        constituent_records_by_table_name[table.global_name] = record
+      end
+      constituent_records_by_table_name.inspect
+    end
+
+    protected
     def build_constituent_record(table, field_values)
       table_specific_field_values = {}
       field_values.each do |field_name, value|
         table_specific_field_name = field_name.to_s.gsub!("#{table.global_name}__", "")
         if table_specific_field_name
-          table_specific_field_values[table_specific_field_name] = value
+          table_specific_field_values[table_specific_field_name.to_sym] = value
         end
       end
-      table.record_class.unsafe_new(table_specific_field_values)
-    end
-
-    def [](table_or_record_class)
-      constituent_records_by_table[table_or_record_class.table]
+      table.build_record_from_database(table_specific_field_values)
     end
   end
 end
