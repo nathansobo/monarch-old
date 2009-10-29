3f26767f6863171eb41fafc6cd18264cc2aff9f3
Jake Frautschi & Nathan Sobo <nathansobo+monarch@gmail.com>
2009-10-29 12:27:32 -0700

    Extracted a Monarch.Http.Command superconstructor from the create, update, and destroy commands

--- client/lib/monarch/http/update_command.js
+++ client/lib/monarch/http/update_command.js
@@ -1,33 +1,25 @@
 (function(Monarch) {
 
-Monarch.constructor("Monarch.Http.UpdateCommand", {
+Monarch.constructor("Monarch.Http.UpdateCommand", Monarch.Http.Command, {
   initialize: function(record, values_by_method_name) {
     this.record = record;
+    this.table_name = this.record.table().global_name;
+    this.command_id = record.id();
     this.values_by_method_name = values_by_method_name;
     this.future = new Monarch.Http.RepositoryUpdateFuture();
-    this.command_id = record.id();
   },
 
-  add_to_request_data: function(request_data) {
-    var table_name = this.record.table().global_name;
-    if (!request_data[table_name]) request_data[table_name] = {};
-
+  wire_representation: function() {
     this.record.start_pending_changes();
     this.record.local_update(this.values_by_method_name);
     this.pending_fieldset = this.record.active_fieldset;
     this.record.restore_primary_fieldset();
-
-    request_data[table_name][this.command_id] = this.pending_fieldset.wire_representation();
+    return this.pending_fieldset.wire_representation();
   },
 
-  complete_and_trigger_before_events: function(field_values_from_server) {
+  complete: function(field_values_from_server) {
     this.pending_fieldset.update(field_values_from_server);
     this.pending_fieldset.commit();
-    this.future.trigger_before_events(this.record);
-  },
-
-  trigger_after_events: function() {
-    this.future.trigger_after_events(this.record);
   }
 });
 
